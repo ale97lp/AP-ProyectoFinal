10fa3b7292c1edacbcb02617b1ee353574748131
ale97lp <jesusllimpe_unlp@hotmail.com>
2022-08-02 18:39:38 -0300

    ed-exp-pro-hys-f

--- ProyectoIntegrador_JesusLlimpe/src/app/app.module.ts
+++ ProyectoIntegrador_JesusLlimpe/src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
-import { SocialComponent } from './components/social/social.component';
+
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './components/hys/hys.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 
 
@@ -17,17 +23,22 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
-    SocialComponent,
+    
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
+    EducacionComponent,
+    HysComponent,
+    ProyectosComponent,
+    FooterComponent,
     
     
     
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
